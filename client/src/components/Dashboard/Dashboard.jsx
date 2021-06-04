1209e235c88b7680499104f1e11f58a463ef9cd0
Dylan <dcren2010@gmail.com>
2021-06-04 10:34:02 -0400

    create useAuth folder but nothing implemented

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Dashboard = ({code}) => {
 	return (
 		<>
+			<h1>Dashboard</h1>
 			<p>{code}</p>
 		</>	
 	 );
---
+++ client/src/useAuth/useAuth.js
@@ -0,0 +1,16 @@
+import { useState, uesEffect } from 'react'
+import axios from 'axios'
+
+export default useAuth = (code) => {
+	const [token, setToken] = useState('')
+	const [refreshToken, setRefreshToken] = useState('')
+	const [expireIn, setExpireIn] = useState('')
+
+	useEffect(() => {
+
+
+
+	}, [])
+
+
+}
