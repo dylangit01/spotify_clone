f4d134f1d5995c3c2a54cca10d62b0c41a4da0fb
Dylan <dcren2010@gmail.com>
2021-06-04 20:17:40 -0400

    build spotify clone

--- server/server.js
+++ server/server.js
@@ -7,12 +7,35 @@ app.use(cors());
 
 app.use(express.json());
 
+app.post('/refresh', (req, res) => {
+	const refreshToken = req.body.refreshToken;
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: 'http://localhost:3000/callback/',
+		clientId: '5ca23191c3a04e9c99be48a4ab6c81dc',
+		clientSecret: 'b9022f56c67c4561a5c091a23d1a2b86',
+		refreshToken,
+	});
+
+	spotifyApi
+		.refreshAccessToken()
+		.then((data) => {
+			res.json({
+				accessToken: data.body.accessToken,
+				expiresIn: data.body.expiresIn,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(400);
+		});
+});
+
 app.post('/login', (req, res) => {
 	const code = req.body.code;
 	const spotifyApi = new SpotifyWebApi({
-		redirectUri: 'http://localhost:3000/callback/',	// Damn the "Url --> Uri", a character kills everything
-		clientId: '2baa05a85a284865a4cdde82479ff3a9',
-		clientSecret: 'b0151a612f7f423d979a4e4c63a2f5b1',
+		redirectUri: 'http://localhost:3000/callback/', // Damn the "Url --> Uri", a character kills everything
+		clientId: '5ca23191c3a04e9c99be48a4ab6c81dc',
+		clientSecret: 'b9022f56c67c4561a5c091a23d1a2b86',
 	});
 
 	spotifyApi
